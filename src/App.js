30166c6a66c7e67519f25140413dde09626f8829
Nemanja2912 <nemanja.mijailovic2912@hotmail.com>
2021-01-04 15:00:22 +0100

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import SideBar from "./components/Navigation/SideBar";
 import JobPage from "./components/Job/JobPage";
 import HomePage from "./components/Home/HomePage";
@@ -8,7 +8,7 @@ import "./style.css";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter basename="/">
         <div>
           <SideBar />
         </div>
@@ -20,7 +20,7 @@ function App() {
             <Route exact path="/" component={HomePage} />
           </Switch>
         </div>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
